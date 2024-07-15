a3f23fd755c27f5e68510e91bab77d1e497c81e5
meganarmas <meganarmas@gmail.com>
2024-07-15 08:06:23 -0500

    now ready to turn in

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 To Do List Project:
 
 -Overview-
+
+Welcome to the To Do List application. Here, users are able to add new tasks to their to do list, view their tasks, mark task as completed, and delete tasks.
+
+To use this application, start at the beginning with the menu. Here, users can use '1' to add a new task, '2' to view their tasks, '3' to mark a task as completed', '4' to delete a task, and '5' to exit out of the menu. 
+
+For marking a task as completed and deleting a task, please input the task number. For example, if you want to delete "3. going to the gym", instead of typing out gym, type "3" and it will either mark the task as completed or delete it.
+
+If a task has been completed, it will say "completed" in green. If not, "incomplete" will pop up in red. In order to have a task marked as completed, type in '3' to go to the completed list. Type in the number of task you want to mark off as completed, and it will pop up as completed.
--- To Do.py
+++ To Do.py
@@ -1,25 +1,41 @@
-current_tasks = ['Walk the dog ' '\033[31m Incomplete', 
-            "\033[37m Laundry " '\033[31m Incomplete',
-            "\033[37m Go on a run " '\033[32m Complete',
-            "\033[37m Clean kitchen " "\033[31m Incomplete"]
+current_tasks = ['\033[37m walk the dog ' '\033[31m Incomplete', 
+            '\033[37m laundry ' '\033[31m Incomplete',
+            '\033[37m go on a run ' '\033[32m Complete',
+            '\033[37m clean kitchen ' "\033[31m Incomplete"]
 
 def add_task(title="Incomplete"):
     if title is not current_tasks:
         current_tasks.append(title + ' Incomplete')
-        print(f"{title} has been added to the to-do list")
+        print(f"\033[37m {title} has been added to the to-do list")
     else:
-        print(f"{title} is already in the to-do list.")
-    
-def remove_task(remove_item):
-        if remove_item in current_tasks:
-            current_tasks.remove(remove_item)
-            print(f"{remove_item} has been removed fom your to do list!")
+        print(f"\033[37m {title} is already in the to-do list.")
+
+def complete_task(mark_complete):
+    mark_complete = []
+    try:
+        index = int(mark_complete) - 1
+        if 0 <= index < len(current_tasks):
+            current_tasks.replace('\033[31m Incomplete', '\033[32m Complete')
+            print(f"Your task has been marked as completed. Great job!")
         else:
-            print("Task is not on the list - please add it first.")
+            ("Invalid.")
+    except TypeError:
+        print("This task has not been added! Please add it first.")
+
+def remove_task(delete_task):
+    try:
+        index = int(delete_task) - 1
+        if 0 <= index < len(current_tasks):
+            del current_tasks[index]
+            print(f"Your task deleted successfully.")
+        else:
+            print("Invalid task number. Please enter a correct number.")
+    except TypeError:
+        print("This task has not been added! Please add it first")
 
 def to_do_list():
         while True:
-            print("\033[37m Would you like to add tasks, view the current to do list, or remove a take?")
+            print("\033[37mWould you like to add tasks, view the current to do list, or remove a take?")
             print("1. Add A Task")
             print("2. View Tasks")
             print("3. Mark Task as Complete")
@@ -34,23 +50,16 @@ def to_do_list():
                 for to_do_task in current_tasks:
                     print(to_do_task)
             elif choice == '3':
-                mark_complete = []
-                try:
-                    mark_complete == current_tasks.replace("Incomplete", "Complete")
-                    if mark_complete in current_tasks:
-                        mark_complete = current_tasks.replace("Incomplete", "\033[32m Complete")
-                        print(f"You have completed {mark_complete}! Great job!")
-                    else:
-                        print("Invalid.")
-                except TypeError:
-                    print("This task has not been added! Please add it first")
+                print("What would you mark off as completed? Please enter a number: ")
+                complete_task()
             elif choice == '4':
-                remove_item = input("Enter what to remove from the to-do list?: ")
-                remove_task()
+                delete_task = input("Which assignment would you like to mark off? Please enter a number: ")
+                remove_task(delete_task)
             elif choice == '5':
                 print("Thanks for using the application! Closing To Do List now!")
                 break
             else:
                 print("Invalid choice. Please select a number from 1 - 5. ")
+
 
 to_do_list()
